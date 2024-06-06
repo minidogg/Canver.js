86dcbfd784317ca15f45d3ecb861b99a6d40aaa5
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-06-06 15:07:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ Canver.Canver = class {
     this.render = { renderFuncs: [] }
     this.render.min = 0
     this.render.max = 0
-    this.tick = { tickFuncs: [], interval: 20 }
+    this.tick = { tickFuncs: [], interval: 1000 / 30 }
 
     Object.keys(options).forEach(e => {
       this[e] = options[e]
@@ -79,35 +79,145 @@ Canver.Canver = class {
 Canver.gameObject = class {
   constructor(options = {}) {
     this.type = "gameObject"
-    this.x = 95
-    this.y = 50
+    this.x = 0
+    this.y = 0
     this.width = 10
     this.height = 10
-    this.color = "black"
+    this.color = "lime"
     this.onRenderFuncs = []
-    this.sizer = "max"
+    this.sizer = "min"
     this.sizerVal = 0
     this.anchor = 0.5
+    this.canver = undefined
+    this.renderData = { x: 0, y: 0, w: 0, h: 0 } //where we store the data calculated by the render function so that the other code can interact with it
 
     Object.keys(options).forEach(e => {
       this[e] = options[e]
     })
   }
   add(c) {
+    this.canver = c
     c.scene.push(this)
     return this
   }
+  remove(c = this.canver) {
+    c.scene.splice(c.scene.indexOf(this), 1)
+  }
   render({ ctx, min, canvas, max }) {
-    this.sizerVal = this.sizer == "max" ? max : min
+    let { w, h, y, x } = { x: 0, y: 0, w: 0, h: 0 }
+    if (this.sizer == "relative") {
+      w = this.width / 100 * this.canver.c.width
+      h = this.height / 100 * this.canver.c.height
+      y = this.y / 100 * canvas.height
+      x = this.x / 100 * canvas.width
+    } else {
+      this.sizerVal = this.sizer == "max" ? max : min
+      w = this.width / 100 * this.sizerVal
+      h = this.height / 100 * this.sizerVal
+      y = this.y / 100 * canvas.height
+      x = this.x / 100 * canvas.width
+    }
     ctx.fillStyle = this.color
-    let w = this.width / 100 * this.sizerVal
-    let h = this.height / 100 * this.sizerVal
-    let y = this.y / 100 * canvas.height
-    let x = this.x / 100 * canvas.width
     ctx.fillRect(x - (w * this.anchor), y - (h * this.anchor), w, h)
     this.onRenderFuncs.forEach(e => {
       e({ ctx: ctx, min: min, canvas: canvas })
     })
   }
 
+  isColliding(obj) {
+    return (
+      this.x < obj.x + obj.width &&
+      this.x + this.width > obj.x &&
+      this.y < obj.y + obj.height &&
+      this.y + this.height > obj.y
+    );
+  }
+
+  touchingEdge() {
+    let { w, h, x, y } = this.renderData
+
+    return (
+      x < 0 ||
+      y < 0 ||
+      x + w > this.canver.c.width ||
+      y + h > this.canver.c.height
+    );
+  }
+}
+
+//dumby object
+Canver.dumby = class extends Canver.gameObject {
+  constructor(options) {
+    super(options)
+  }
+  render({ ctx, min, canvas, max }) {
+    this.onRenderFuncs.forEach(e => {
+      e({ ctx, min, canvas, max })
+    })
+  }
+}
+
+//sprite class
+Canver.sprite = class extends Canver.gameObject {
+  registerImg(url) {
+    this.img = new Image()
+    this.img.src = url
+    return this
+  }
+  render({ ctx, min, canvas, max }) {
+
+    let { w, h, y, x } = { x: 0, y: 0, w: 0, h: 0 }
+    if (this.sizer == "relative") {
+      w = this.width / 100 * canvas.width
+      h = this.height / 100 * canvas.height
+      y = this.y / 100 * canvas.height
+      x = this.x / 100 * canvas.width
+    } else {
+      this.sizerVal = this.sizer == "max" ? max : min
+      w = this.width / 100 * this.sizerVal
+      h = this.height / 100 * this.sizerVal
+      y = this.y / 100 * canvas.height
+      x = this.x / 100 * canvas.width
+    }
+    ctx.fillStyle = this.color
+    this.renderData = { w, h, x, y }
+    ctx.drawImage(this.img, x - (w * this.anchor), y - (h * this.anchor), w, h)
+    this.onRenderFuncs.forEach(e => {
+      e({ ctx: ctx, min: min, canvas: canvas })
+    })
+  }
+
+}
+
+///text class
+Canver.text = class extends Canver.gameObject {
+  constructor(options) {
+    super()
+    this.text = "Hello, World!"
+    this.font = "Arial"
+    this.fontSize = 3
+    this.sizerVal = "max"
+    this.x = 50
+    this.y = 5
+    this.color = "black"
+    this.textAlign = "left"
+
+    Object.keys(options).forEach(e => {
+      this[e] = options[e]
+    })
+  }
+
+  render({ ctx, min, canvas, max }) {
+    let { w, h, y, x } = { x: 0, y: 0, w: 0, h: 0 }
+
+    this.sizerVal = this.sizer == "max" ? max : min
+    w = this.width / 100 * this.sizerVal
+    y = this.y / 100 * canvas.height
+    x = this.x / 100 * canvas.width
+
+    ctx.font = this.fontSize + "v" + this.sizer + " " + this.font
+    ctx.fillStyle = this.color
+    ctx.textAlign = this.textAlign
+    ctx.fillText(this.text, x - (w * this.anchor), y - (h * this.anchor));
+  }
 }
